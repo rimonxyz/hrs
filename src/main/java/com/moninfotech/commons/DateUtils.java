e071f1174aacf896be87b06f5fc87f3b29ec7678
Sayem Hossain <sayemkcn@gmail.com>
2017-04-21 17:36:18 +0600

    Fixed search algorithm issue, search works

--- src/main/java/com/moninfotech/commons/DateUtils.java
+++ src/main/java/com/moninfotech/commons/DateUtils.java
@@ -7,6 +7,6 @@ import java.util.Date;
  */
 public class DateUtils {
     public static boolean isInBetween(Date startDate, Date endDate, Date date1, Date date2) {
-        return date1.compareTo(startDate) * endDate.compareTo(date1) > 0 || date2.compareTo(startDate) * endDate.compareTo(date2) > 0;
+        return date1.compareTo(startDate) * endDate.compareTo(date1) >= 0 || date2.compareTo(startDate) * endDate.compareTo(date2) >= 0;
     }
 }
