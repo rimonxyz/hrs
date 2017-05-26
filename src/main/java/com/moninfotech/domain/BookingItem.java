e3ed0dc4729e00c35454191a95bfe80c430c0365
Sayem Hossain <sayemkcn@gmail.com>
2017-05-26 16:19:31 +0600

    added booking item class

---
+++ src/main/java/com/moninfotech/domain/BookingItem.java
@@ -0,0 +1,12 @@
+package com.moninfotech.domain;
+
+import java.util.Date;
+
+/**
+ * Created by sayemkcn on 5/26/17.
+ */
+
+public class BookingItem {
+    private Room room;
+    private Date bookingDate;
+}
