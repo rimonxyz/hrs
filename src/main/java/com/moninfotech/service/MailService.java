43b0f93285b99a72900e71f655febf4b07eb2476
Sayem Hossain <sayemkcn@gmail.com>
2018-10-10 15:11:51 +0600

    Reverted to deployment db config

--- src/main/java/com/moninfotech/service/MailService.java
+++ src/main/java/com/moninfotech/service/MailService.java
@@ -1,9 +1,5 @@
 package com.moninfotech.service;
 
-import com.sun.mail.util.MailConnectException;
-
-import java.net.UnknownHostException;
-
 public interface MailService {
 
     void sendEmail(String email, String subject, String message);
